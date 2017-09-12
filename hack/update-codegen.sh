b0b050377b00e38e0590182bd502c9dc46b024dc
David Eads <deads@redhat.com>
2017-09-12 18:23:11 -0400

    update to generate all clients

--- hack/update-codegen.sh
+++ hack/update-codegen.sh
@@ -5,8 +5,19 @@ go build -o _output/bin/client-gen github.com/openshift/client-go/vendor/k8s.io/
 
 # list of package to generate client set for
 packages=(
+  github.com/openshift/origin/pkg/apps/apis/apps
+  github.com/openshift/origin/pkg/authorization/apis/authorization
+  github.com/openshift/origin/pkg/build/apis/build
+  github.com/openshift/origin/pkg/image/apis/image
+  github.com/openshift/origin/pkg/network/apis/network
+  github.com/openshift/origin/pkg/oauth/apis/oauth
+  github.com/openshift/origin/pkg/project/apis/project
+  github.com/openshift/origin/pkg/quota/apis/quota
+  github.com/openshift/origin/pkg/route/apis/route
+  github.com/openshift/origin/pkg/security/apis/security
   github.com/openshift/origin/pkg/template/apis/template
 )
+
 
 function generate_clientset_for() {
   local package="$1";shift
