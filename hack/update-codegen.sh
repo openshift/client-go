4ec604c4b59b075c5793034af0c80f328c62e1b2
Dr. Stefan Schimanski <stefan.schimanski@gmail.com>
2017-11-21 11:44:37 +0100

    Turn hack/update-codegen.sh into a loop over the groups

--- hack/update-codegen.sh
+++ hack/update-codegen.sh
@@ -7,75 +7,10 @@ set -o pipefail
 SCRIPT_ROOT=$(dirname ${BASH_SOURCE})/..
 CODEGEN_PKG=${CODEGEN_PKG:-$(cd ${SCRIPT_ROOT}; ls -d -1 ./vendor/k8s.io/code-generator 2>/dev/null || echo ../../../k8s.io/code-generator)}
 
-${CODEGEN_PKG}/generate-groups.sh "client,lister,informer" \
-  github.com/openshift/client-go/apps \
-  github.com/openshift/api \
-  "apps:v1" \
-  --go-header-file ${SCRIPT_ROOT}/hack/boilerplate.txt
-
-${CODEGEN_PKG}/generate-groups.sh "client,lister,informer" \
-  github.com/openshift/client-go/authorization \
-  github.com/openshift/api \
-  "authorization:v1" \
-  --go-header-file ${SCRIPT_ROOT}/hack/boilerplate.txt
-
-${CODEGEN_PKG}/generate-groups.sh "client,lister,informer" \
-  github.com/openshift/client-go/build \
-  github.com/openshift/api \
-  "build:v1" \
-  --go-header-file ${SCRIPT_ROOT}/hack/boilerplate.txt
-
-${CODEGEN_PKG}/generate-groups.sh "client,lister,informer" \
-  github.com/openshift/client-go/image \
-  github.com/openshift/api \
-  "image:v1" \
-  --go-header-file ${SCRIPT_ROOT}/hack/boilerplate.txt
-
-${CODEGEN_PKG}/generate-groups.sh "client,lister,informer" \
-  github.com/openshift/client-go/network \
-  github.com/openshift/api \
-  "network:v1" \
-  --go-header-file ${SCRIPT_ROOT}/hack/boilerplate.txt
-
-${CODEGEN_PKG}/generate-groups.sh "client,lister,informer" \
-  github.com/openshift/client-go/oauth \
-  github.com/openshift/api \
-  "oauth:v1" \
-  --go-header-file ${SCRIPT_ROOT}/hack/boilerplate.txt
-
-${CODEGEN_PKG}/generate-groups.sh "client,lister,informer" \
-  github.com/openshift/client-go/project \
-  github.com/openshift/api \
-  "project:v1" \
-  --go-header-file ${SCRIPT_ROOT}/hack/boilerplate.txt
-
-${CODEGEN_PKG}/generate-groups.sh "client,lister,informer" \
-  github.com/openshift/client-go/quota \
-  github.com/openshift/api \
-  "quota:v1" \
-  --go-header-file ${SCRIPT_ROOT}/hack/boilerplate.txt
-
-${CODEGEN_PKG}/generate-groups.sh "client,lister,informer" \
-  github.com/openshift/client-go/route \
-  github.com/openshift/api \
-  "route:v1" \
-  --go-header-file ${SCRIPT_ROOT}/hack/boilerplate.txt
-
-${CODEGEN_PKG}/generate-groups.sh "client,lister,informer" \
-  github.com/openshift/client-go/security \
-  github.com/openshift/api \
-  "security:v1" \
-  --go-header-file ${SCRIPT_ROOT}/hack/boilerplate.txt
-
-${CODEGEN_PKG}/generate-groups.sh "client,lister,informer" \
-  github.com/openshift/client-go/template \
-  github.com/openshift/api \
-  "template:v1" \
-  --go-header-file ${SCRIPT_ROOT}/hack/boilerplate.txt
-
-${CODEGEN_PKG}/generate-groups.sh "client,lister,informer" \
-  github.com/openshift/client-go/user \
-  github.com/openshift/api \
-  "user:v1" \
-  --go-header-file ${SCRIPT_ROOT}/hack/boilerplate.txt
-
+for group in apps authorization build image network oauth project quota route security template user; do
+  ${CODEGEN_PKG}/generate-groups.sh "client,lister,informer" \
+    github.com/openshift/client-go/${group} \
+    github.com/openshift/api \
+    "${group}:v1" \
+    --go-header-file ${SCRIPT_ROOT}/hack/boilerplate.txt
+done
