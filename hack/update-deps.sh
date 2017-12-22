a627f8582c05443e68f41cdc8e7d894fcfcf62c0
Maciej Szulik <maszulik@redhat.com>
2017-12-22 12:10:44 +0100

    Check glide version

--- hack/update-deps.sh
+++ hack/update-deps.sh
@@ -1,4 +1,23 @@
 #!/bin/bash -e
 
+readonly REQUIRED_GLIDE_VERSION="v0.13"
+
+function verify_glide_version() {
+	if ! command -v glide &> /dev/null; then
+		echo "[FATAL] Glide was not found in \$PATH. Please install version ${REQUIRED_GLIDE_VERSION} or newer."
+		exit 1
+	fi
+
+	local glide_version
+	glide_version=($(glide --version))
+	if [[ "${glide_version[2]}" < "${REQUIRED_GLIDE_VERSION}" ]]; then
+		echo "Detected glide version: ${glide_version[*]}."
+		echo "Please install Glide version ${REQUIRED_GLIDE_VERSION} or newer."
+		exit 1
+	fi
+}
+
+verify_glide_version
+
 glide update --strip-vendor
 
