272e2944b2a7d7ecc1ed50f771c639db0faa70eb
Ben Parees <bparees@redhat.com>
2019-01-29 18:23:26 -0500

    bump(*)

--- vendor/github.com/openshift/api/config/v1/types_infrastructure.go
+++ vendor/github.com/openshift/api/config/v1/types_infrastructure.go
@@ -7,7 +7,6 @@ import metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
 
 // Infrastructure holds cluster-wide information about Infrastructure.  The canonical name is `cluster`
-// TODO this object is an example of a possible grouping and is subject to change or removal
 type Infrastructure struct {
 	metav1.TypeMeta `json:",inline"`
 	// Standard object's metadata.
@@ -19,39 +18,54 @@ type Infrastructure struct {
 	Status InfrastructureStatus `json:"status"`
 }
 
+// InfrastructureSpec contains settings that apply to the cluster infrastructure.
 type InfrastructureSpec struct {
 	// secret reference?
 	// configmap reference to file?
 }
 
+// InfrastructureStatus describes the infrastructure the cluster is leveraging.
 type InfrastructureStatus struct {
 	// platform is the underlying infrastructure provider for the cluster. This
 	// value controls whether infrastructure automation such as service load
 	// balancers, dynamic volume provisioning, machine creation and deletion, and
 	// other integrations are enabled. If None, no infrastructure automation is
-	// enabled.
+	// enabled. Allowed values are "AWS", "Azure", "GCP", "Libvirt",
+	// "OpenStack", "VSphere", and "None". Individual components may not support
+	// all platforms, and must handle unrecognized platforms as None if they do
+	// not support that platform.
 	Platform PlatformType `json:"platform,omitempty"`
 }
 
-// platformType is a specific supported infrastructure provider.
+// PlatformType is a specific supported infrastructure provider.
 type PlatformType string
 
 const (
-	// awsPlatform represents Amazon AWS.
+	// AWSPlatform represents Amazon Web Services infrastructure.
 	AWSPlatform PlatformType = "AWS"
 
-	// openStackPlatform represents OpenStack.
-	OpenStackPlatform PlatformType = "OpenStack"
+	// AzurePlatform represents Microsoft Azure infrastructure.
+	AzurePlatform PlatformType = "Azure"
 
-	// libvirtPlatform represents libvirt.
+	// GCPPlatform represents Google Cloud Platform infrastructure.
+	GCPPlatform PlatformType = "GCP"
+
+	// LibvirtPlatform represents libvirt infrastructure.
 	LibvirtPlatform PlatformType = "Libvirt"
 
-	// nonePlatform means there is no infrastructure provider.
+	// OpenStackPlatform represents OpenStack infrastructure.
+	OpenStackPlatform PlatformType = "OpenStack"
+
+	// NonePlatform means there is no infrastructure provider.
 	NonePlatform PlatformType = "None"
+
+	// VSpherePlatform represents VMWare vSphere infrastructure.
+	VSpherePlatform PlatformType = "VSphere"
 )
 
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
 
+// InfrastructureList is
 type InfrastructureList struct {
 	metav1.TypeMeta `json:",inline"`
 	// Standard object's metadata.
