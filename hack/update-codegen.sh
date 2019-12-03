87134d1f756557e484b64fb7264e7850c7b7c72f
Ricardo Maraschini <rmarasch@redhat.com>
2019-12-03 19:53:32 +0100

    adjusted hack scripts

--- hack/update-codegen.sh
+++ hack/update-codegen.sh
@@ -4,6 +4,7 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
+export GO111MODULE=on
 SCRIPT_ROOT=$(dirname ${BASH_SOURCE})/..
 CODEGEN_PKG=${CODEGEN_PKG:-$(cd ${SCRIPT_ROOT}; ls -d -1 ./vendor/k8s.io/code-generator 2>/dev/null || echo ../../../k8s.io/code-generator)}
 
