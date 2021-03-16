426732b976f06a68dafe5efa978b8d79bb2b8702
Maciej Szulik <maszulik@redhat.com>
2021-03-16 16:31:11 +0100

    bump(k8s.io/*): v0.21.0-rc.0

--- vendor/github.com/openshift/api/config/v1/types_feature.go
+++ vendor/github.com/openshift/api/config/v1/types_feature.go
@@ -105,7 +105,10 @@ var FeatureSets = map[FeatureSet]*FeatureGateEnabledDisabled{
 		Enabled:  []string{},
 		Disabled: []string{},
 	},
-	TechPreviewNoUpgrade: newDefaultFeatures().toFeatures(),
+	TechPreviewNoUpgrade: newDefaultFeatures().
+		with("CSIDriverAzureDisk"). // sig-storage, jsafrane
+		with("CSIDriverVSphere").   // sig-storage, jsafrane
+		toFeatures(),
 	LatencySensitive: newDefaultFeatures().
 		with(
 			"TopologyManager", // sig-pod, sjenning
@@ -125,11 +128,10 @@ var defaultFeatures = &FeatureGateEnabledDisabled{
 		"SupportPodPidsLimit",            // sig-pod, sjenning
 		"NodeDisruptionExclusion",        // sig-scheduling, ccoleman
 		"ServiceNodeExclusion",           // sig-scheduling, ccoleman
-		"SCTPSupport",                    // sig-network, ccallend
+		"DownwardAPIHugePages",           // sig-node, rphillips
 	},
 	Disabled: []string{
 		"LegacyNodeRoleBehavior", // sig-scheduling, ccoleman
-		"RemoveSelfLink",         // kuryr needs updating, deads2k will personally remove in 4.8
 	},
 }
 
