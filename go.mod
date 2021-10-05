bdf04004f15686adf866eb9d479992e97475a324
Corey Daley <cdaley@redhat.com>
2021-10-05 15:29:05 -0400

    bump(*)

--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module github.com/openshift/client-go
 go 1.16
 
 require (
-	github.com/openshift/api v0.0.0-20211005161005-4ec2bbe5ed00
+	github.com/openshift/api v0.0.0-20211007134530-4cb30f221b89
 	github.com/spf13/pflag v1.0.5
 	k8s.io/api v0.22.1
 	k8s.io/apimachinery v0.22.1
--- vendor/github.com/openshift/api/config/v1/zz_generated.swagger_doc_generated.go
+++ vendor/github.com/openshift/api/config/v1/zz_generated.swagger_doc_generated.go
@@ -1729,7 +1729,7 @@ var map_ProxySpec = map[string]string{
 	"":                   "ProxySpec contains cluster proxy creation configuration.",
 	"httpProxy":          "httpProxy is the URL of the proxy for HTTP requests.  Empty means unset and will not result in an env var.",
 	"httpsProxy":         "httpsProxy is the URL of the proxy for HTTPS requests.  Empty means unset and will not result in an env var.",
-	"noProxy":            "noProxy is a comma-separated list of hostnames and/or CIDRs for which the proxy should not be used. Empty means unset and will not result in an env var.",
+	"noProxy":            "noProxy is a comma-separated list of hostnames and/or CIDRs and/or IPs for which the proxy should not be used. Empty means unset and will not result in an env var.",
 	"readinessEndpoints": "readinessEndpoints is a list of endpoints used to verify readiness of the proxy.",
 	"trustedCA":          "trustedCA is a reference to a ConfigMap containing a CA certificate bundle. The trustedCA field should only be consumed by a proxy validator. The validator is responsible for reading the certificate bundle from the required key \"ca-bundle.crt\", merging it with the system default trust bundle, and writing the merged trust bundle to a ConfigMap named \"trusted-ca-bundle\" in the \"openshift-config-managed\" namespace. Clients that expect to make proxy connections must use the trusted-ca-bundle for all HTTPS requests to the proxy, and may use the trusted-ca-bundle for non-proxy HTTPS requests as well.\n\nThe namespace for the ConfigMap referenced by trustedCA is \"openshift-config\". Here is an example ConfigMap (in yaml):\n\napiVersion: v1 kind: ConfigMap metadata:\n name: user-ca-bundle\n namespace: openshift-config\n data:\n   ca-bundle.crt: |",
 }
