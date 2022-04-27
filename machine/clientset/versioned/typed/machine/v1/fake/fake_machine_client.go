ab146d908ef8f2802278a8fb197671ddb57e188d
Damiano Donati <damiano.donati@gmail.com>
2022-04-27 15:05:28 +0100

    Update generated client for Machine v1

--- machine/clientset/versioned/typed/machine/v1/fake/fake_machine_client.go
+++ machine/clientset/versioned/typed/machine/v1/fake/fake_machine_client.go
@@ -3,17 +3,12 @@
 package fake
 
 import (
-	v1 "github.com/openshift/client-go/machine/clientset/versioned/typed/machine/v1"
 	rest "k8s.io/client-go/rest"
 	testing "k8s.io/client-go/testing"
 )
 
 type FakeMachineV1 struct {
 	*testing.Fake
-}
-
-func (c *FakeMachineV1) AWSPlacementGroups(namespace string) v1.AWSPlacementGroupInterface {
-	return &FakeAWSPlacementGroups{c, namespace}
 }
 
 // RESTClient returns a RESTClient that is used to communicate
