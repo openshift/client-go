b276847abdbc8bb10ccc6f99be269970c06f834f
Maciej Szulik <maszulik@redhat.com>
2024-02-20 14:55:33 +0100

    Update verify-codegen.sh

--- hack/verify-codegen.sh
+++ hack/verify-codegen.sh
@@ -5,4 +5,14 @@ set -o nounset
 set -o pipefail
 
 SCRIPT_ROOT=$(dirname ${BASH_SOURCE})/..
-VERIFY=--verify-only ${SCRIPT_ROOT}/hack/update-codegen.sh
+
+"${SCRIPT_ROOT}/hack/update-codegen.sh"
+
+ret=0
+git diff --exit-code --quiet || ret=$?
+if [[ $ret -ne 0 ]]; then
+  echo "Codegen is out of date. Please run hack/update-codegen.sh"
+  exit 1
+fi
+echo "Codegen up to date."
+
