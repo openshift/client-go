52d16d91a91765c3c44f8911c38b85d18519f0c2
kevinrizza <krizza@redhat.com>
2025-04-11 11:08:31 -0400

    bump(k8s): make update

--- console/informers/externalversions/console/v1/consolequickstart.go
+++ console/informers/externalversions/console/v1/consolequickstart.go
@@ -45,13 +45,25 @@ func NewFilteredConsoleQuickStartInformer(client versioned.Interface, resyncPeri
 				if tweakListOptions != nil {
 					tweakListOptions(&options)
 				}
-				return client.ConsoleV1().ConsoleQuickStarts().List(context.TODO(), options)
+				return client.ConsoleV1().ConsoleQuickStarts().List(context.Background(), options)
 			},
 			WatchFunc: func(options metav1.ListOptions) (watch.Interface, error) {
 				if tweakListOptions != nil {
 					tweakListOptions(&options)
 				}
-				return client.ConsoleV1().ConsoleQuickStarts().Watch(context.TODO(), options)
+				return client.ConsoleV1().ConsoleQuickStarts().Watch(context.Background(), options)
+			},
+			ListWithContextFunc: func(ctx context.Context, options metav1.ListOptions) (runtime.Object, error) {
+				if tweakListOptions != nil {
+					tweakListOptions(&options)
+				}
+				return client.ConsoleV1().ConsoleQuickStarts().List(ctx, options)
+			},
+			WatchFuncWithContext: func(ctx context.Context, options metav1.ListOptions) (watch.Interface, error) {
+				if tweakListOptions != nil {
+					tweakListOptions(&options)
+				}
+				return client.ConsoleV1().ConsoleQuickStarts().Watch(ctx, options)
 			},
 		},
 		&apiconsolev1.ConsoleQuickStart{},
